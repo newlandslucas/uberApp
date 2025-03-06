778a85dac99bbbed5dff75a2bc4b4584f383d70b
newlandslucas <lucasnewlands.m@gmail.com>
2025-03-06 00:49:33 -0300

    add root | auth screens setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,8 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </GestureHandlerRootView>
